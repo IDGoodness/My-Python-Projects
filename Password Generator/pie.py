b45cfa6a6eb3cf615abc9a52cf4ffe81d00e3263
Goodness Adewuyi <80427211+IDGoodness@users.noreply.github.com>
2022-06-16 23:09:05 +0100

    Add files via upload

---
+++ Password Generator/pie.py
@@ -0,0 +1,31 @@
+def generate():
+    from operator import length_hint
+    import random
+
+    lowerCase = "qwertyuiopasdfghjklzxcvbnm"
+    upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    number = "0123456789"
+    symbols = '''`~!@#$%^&*()_+-=|\"'{}[]:;?/>.<,'''
+
+    UseFor = lowerCase + upperCase + number + symbols
+    length_for_pass = 8
+
+    password = "".join(random.sample(UseFor, length_for_pass))
+
+    print("Your generated password: "+password)
+    result.element.text = generate()
+
+# languages = [
+#     "Python",
+#     "C++",
+#     "JavaScript",
+#     "Ruby",
+#     "PHP"
+# ]
+
+# print(len(languages))
+
+
+
+
+
